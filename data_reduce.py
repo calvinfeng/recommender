6a1747e5c58abb9fe069e6355311597438bdf79f
Calvin <calvinjhfeng@gmail.com>
2016-09-18 00:33:11 -0700

    separate year from movie title and make it an individual column in csv file

--- data_reduce.py
+++ data_reduce.py
@@ -9,13 +9,18 @@ LINKS_SET = './ml-latest/links.csv'
 def reduce_movies_file(movie_set):
     movies_csv = open(MOVIE_SET)
     csv_reader = csv.reader(movies_csv)
-    resized_movies_csv = open('./ml-latest-med/movies.csv', 'wt')
+    resized_movies_csv = open('./knn-5k-users/movies.csv', 'wt')
     csv_writer = csv.writer(resized_movies_csv)
-    csv_writer.writerow(('movieId', 'title', 'genres'))
+    csv_writer.writerow(('movieId', 'title', 'year', 'genres'))
     write_count = 0
     for row in csv_reader:
         if row[0].isdigit() and row[0] in movie_set:
-            csv_writer.writerow((row[0], row[1], row[2]))
+            movie_id = row[0]
+            title = row[1].strip()
+            year = title[len(title) - 5: len(title) - 1]
+            genres = row[2]
+            title = title[:len(title) - 6].strip()
+            csv_writer.writerow((movie_id, title, year, genres))
             write_count += 1
     print "Wrote %s items to movies.csv" % write_count
 
@@ -23,7 +28,7 @@ def reduce_movies_file(movie_set):
 def reduce_ratings_file(user_set):
     ratings_csv = open(RATING_SET)
     csv_reader = csv.reader(ratings_csv)
-    resized_ratings_csv = open('./ml-latest-med/ratings.csv', 'wt')
+    resized_ratings_csv = open('./knn-5k-users/ratings.csv', 'wt')
     csv_writer = csv.writer(resized_ratings_csv)
     csv_writer.writerow(('userId', 'movieId', 'rating', 'timestamp'))
     write_count = 0
@@ -40,7 +45,7 @@ def reduce_ratings_file(user_set):
 def reduce_links_file(movie_set):
     links_csv = open(LINKS_SET)
     csv_reader = csv.reader(links_csv)
-    resized_links_csv = open('./ml-latest-med/links.csv', 'wt')
+    resized_links_csv = open('./knn-5k-users/links.csv', 'wt')
     csv_writer = csv.writer(resized_links_csv)
     csv_writer.writerow(('movieId', 'imdbId', 'tmdbId'))
     write_count = 0
